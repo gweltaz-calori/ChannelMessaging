fdb182684705c7379554831e6ffc94a8b2691369
gweltaz-calori <andoracontact@gmail.com>
2017-01-23 10:31:28 +0100

    Ajout images

--- app/src/main/java/gweltaz/calori/channelmessaging/DownloadImageTask.java
+++ app/src/main/java/gweltaz/calori/channelmessaging/DownloadImageTask.java
@@ -3,36 +3,73 @@ package gweltaz.calori.channelmessaging;
 import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
 import android.os.AsyncTask;
+import android.os.Environment;
 import android.util.Log;
 import android.widget.ImageView;
+import android.widget.Toast;
 
+import java.io.BufferedInputStream;
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.IOException;
 import java.io.InputStream;
+import java.net.URL;
+import java.net.URLConnection;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+import de.hdodenhof.circleimageview.CircleImageView;
 
 /**
  * Created by gwel7_000 on 21/01/2017.
  */
 
 public class DownloadImageTask extends AsyncTask<String, Void, Bitmap> {
-    ImageView bmImage;
+    CircleImageView bmImage;
+    String imageurl;
 
-    public DownloadImageTask(ImageView bmImage) {
+    public DownloadImageTask(CircleImageView bmImage,String imageurl) {
         this.bmImage = bmImage;
+        this.imageurl = imageurl;
     }
 
     protected Bitmap doInBackground(String... urls) {
-        String urldisplay = urls[0];
         Bitmap mIcon11 = null;
+        String chemin = imageurl.substring(imageurl.lastIndexOf('/'));
         try {
-            InputStream in = new java.net.URL(urldisplay).openStream();
-            mIcon11 = BitmapFactory.decodeStream(in);
-        } catch (Exception e) {
-            Log.e("Error", e.getMessage());
-            e.printStackTrace();
+            URL url = new URL(urls[0]);
+            File file = new File(Environment.getExternalStorageDirectory()+chemin);
+            file.createNewFile();
+
+            URLConnection ucon = url.openConnection();
+/* Define InputStreams to read from the URLConnection.*/
+            InputStream is = ucon.getInputStream();
+/* Read bytes to the Buffer until there is nothing more to read(-1) and
+write on the fly in the file.*/
+            FileOutputStream fos = new FileOutputStream(file);
+            final int BUFFER_SIZE = 23 * 1024;
+            BufferedInputStream bis = new BufferedInputStream(is, BUFFER_SIZE);
+            byte[] baf = new byte[BUFFER_SIZE];
+            int actual = 0;
+            while (actual != -1) {
+                fos.write(baf, 0, actual);
+                actual = bis.read(baf, 0, BUFFER_SIZE);
+            }
+            fos.close();
+            mIcon11 = BitmapFactory.decodeStream(is);
+        } catch (IOException e) {
+//TODO HANDLER
         }
         return mIcon11;
     }
 
-    protected void onPostExecute(Bitmap result) {
+
+    protected void onPostExecute(Bitmap result)
+    {
+
         bmImage.setImageBitmap(result);
+
+
+
     }
 }
--- app/src/main/java/gweltaz/calori/channelmessaging/MessageListAdapter.java
+++ app/src/main/java/gweltaz/calori/channelmessaging/MessageListAdapter.java
@@ -1,6 +1,9 @@
 package gweltaz.calori.channelmessaging;
 
 import android.content.Context;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.os.Environment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -8,7 +11,10 @@ import android.widget.ArrayAdapter;
 import android.widget.ImageView;
 import android.widget.TextView;
 
+import java.io.File;
 import java.util.List;
+
+import de.hdodenhof.circleimageview.CircleImageView;
 
 /**
  * Created by gwel7_000 on 21/01/2017.
@@ -33,9 +39,29 @@ public class MessageListAdapter extends ArrayAdapter<Message>
 
         TextView titlemessage = (TextView) convertView.findViewById(R.id.titlemessage);
         TextView personmessage = (TextView) convertView.findViewById(R.id.personmessage);
+        CircleImageView avatar = (CircleImageView) convertView.findViewById(R.id.avatarmessage);
+        String chemin = message.getImageUrl().substring(message.getImageUrl().lastIndexOf('/'));
 
-        new DownloadImageTask((ImageView) convertView.findViewById(R.id.avatarmessage))
-                .execute(message.getImageUrl());
+        String path = Environment.getExternalStorageDirectory()+chemin;
+
+        File imgFile = new File(path);
+        System.out.println(imgFile.getPath());
+        if(imgFile.exists())
+        {
+            System.out.println("le fichier existe");
+            Bitmap myBitmap = BitmapFactory.decodeFile(imgFile.getAbsolutePath());
+
+            avatar.setImageBitmap(myBitmap);
+        }
+        else
+        {
+            new DownloadImageTask(avatar,message.getImageUrl())
+                    .execute(message.getImageUrl());
+            System.out.println("le fichier existe pas");
+
+        }
+
+
         titlemessage.setText(message.getMessage());
         personmessage.setText(""+message.getUserID());
 
