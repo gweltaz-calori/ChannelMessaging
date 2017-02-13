5d001afd24a8be51d6832494fede76877b20fcdc
gweltaz-calori <andoracontact@gmail.com>
2017-02-13 11:06:40 +0100

    ajout gps

--- app/src/main/java/gweltaz/calori/channelmessaging/GPSActivity.java
+++ app/src/main/java/gweltaz/calori/channelmessaging/GPSActivity.java
@@ -1,31 +1,31 @@
 package gweltaz.calori.channelmessaging;
 
+
 import android.*;
+import android.Manifest;
 import android.content.pm.PackageManager;
+
+
 import android.location.Location;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
 import android.support.v4.app.ActivityCompat;
 import android.support.v7.app.ActionBarActivity;
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.widget.Toast;
 
 import com.google.android.gms.common.ConnectionResult;
 import com.google.android.gms.common.api.GoogleApiClient;
-import com.google.android.gms.drive.Drive;
 import com.google.android.gms.location.LocationListener;
-
-
 import com.google.android.gms.location.LocationRequest;
 import com.google.android.gms.location.LocationServices;
 
 
-
-
-public class GPSActivity extends ActionBarActivity {
+public class GPSActivity extends ActionBarActivity implements LocationListener {
     private final int MY_PERMISSIONS_REQUEST_LOCATION = 2;
     private GoogleApiClient mGoogleApiClient;
+    private LocationRequest mLocationRequest;
+    private boolean active = false;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -36,14 +36,17 @@ public class GPSActivity extends ActionBarActivity {
                 .addConnectionCallbacks(new GoogleApiClient.ConnectionCallbacks() {
                     @Override
                     public void onConnected(@Nullable Bundle bundle) {
-                        LocationRequest mLocationRequest = new LocationRequest();
+                        mLocationRequest = new LocationRequest();
                         mLocationRequest.setInterval(10000);
                         //Correspond à l’intervalle moyen de temps entre chaque mise à jour des coordonnées
                         mLocationRequest.setFastestInterval(5000);
                         //Correspond à l’intervalle le plus rapide entre chaque mise à jour des coordonnées
                         mLocationRequest.setPriority(LocationRequest.PRIORITY_HIGH_ACCURACY);
                         //Définit la demande de mise à jour avec un niveau de précision maximal
-                        Location lastLocation  = LocationServices.FusedLocationApi.requestLocationUpdates(mGoogleApiClient, mLocationRequest, this);
+
+                        //noinspection MissingPermission
+                        LocationServices.FusedLocationApi.requestLocationUpdates(mGoogleApiClient, mLocationRequest, GPSActivity.this);
+                        active = true;
 
 
 
@@ -68,7 +71,16 @@ public class GPSActivity extends ActionBarActivity {
 
 
 
-        ActivityCompat.requestPermissions(GPSActivity.this,new String[]{android.Manifest.permission.ACCESS_COARSE_LOCATION}, MY_PERMISSIONS_REQUEST_LOCATION);
+        if (ActivityCompat.checkSelfPermission(this, android.Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(this, android.Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+            ActivityCompat.requestPermissions(GPSActivity.this,new String[]{Manifest.permission.ACCESS_FINE_LOCATION}, MY_PERMISSIONS_REQUEST_LOCATION);
+            //    ActivityCompat#requestPermissions
+            // here to request the missing permissions, and then overriding
+            //   public void onRequestPermissionsResult(int requestCode, String[] permissions,
+            //                                          int[] grantResults)
+            // to handle the case where the user grants the permission. See the documentation
+            // for ActivityCompat#requestPermissions for more details.
+            return;
+        }
 
 
 
@@ -102,4 +114,29 @@ public class GPSActivity extends ActionBarActivity {
     }
 
 
+    @Override
+    public void onLocationChanged(Location location)
+    {
+        System.out.println("aeaeoaeaonjeiuaeuijhaehauheaiheiaaheauihehaha");
+        System.out.println(location);
+    }
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+        LocationServices.FusedLocationApi.removeLocationUpdates(
+                mGoogleApiClient, this);
+        active = false;
+
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        if(!active && mGoogleApiClient.isConnected())
+        {
+            //noinspection MissingPermission
+            LocationServices.FusedLocationApi.requestLocationUpdates(mGoogleApiClient, mLocationRequest, this);
+        }
+    }
 }
